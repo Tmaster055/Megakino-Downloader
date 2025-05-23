80a72cf28778eba079cdba6453da1560738c0db8
Tmaster055 <gratzer.tobias3@gmail.com>
2025-05-23 14:55:33 +0200

    Add path argument

--- megakino/src/download.py
+++ megakino/src/download.py
@@ -1,15 +1,14 @@
 import os
-import pathlib
 import subprocess
 
-DEFAULT_DOWNLOAD_PATH = pathlib.Path.home() / "Downloads"
+from .parser import args
 
 
 def download(direct_links, titles):
     counter = 0
     for link in direct_links:
         title = titles[counter]
-        output_file = os.path.join(DEFAULT_DOWNLOAD_PATH, title, f"{title}.mp4")
+        output_file = os.path.join(args.path, title, f"{title}.mp4")
         counter += 1
         command = [
             "yt-dlp",
