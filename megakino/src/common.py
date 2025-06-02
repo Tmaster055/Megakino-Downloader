7149fe65c54b6e7e1fb73d8fc88cf8c68ad41ec8
Tmaster055 <gratzer.tobias3@gmail.com>
2025-06-02 22:07:22 +0200

    Made series work again

--- megakino/src/common.py
+++ megakino/src/common.py
@@ -22,29 +22,38 @@ def get_html_from_search():
 def get_megakino_episodes(soup):
     iframe_tag = soup.find('iframe', src=True)
     if iframe_tag:
-        List = []
-        gxplayer_link = iframe_tag['src']
-        List.append(gxplayer_link)
-        return List
-
-
-def get_voe_episodes(soup):
-    select_tags = soup.find_all('select', class_='mr-select')
-    episode_links = [select.find('option')['value'] for select in select_tags]
-    if episode_links:
-        return episode_links
-
-    voe_links = []
-    for iframe in soup.find_all('iframe', attrs={'data-src': True}):
-        data_src = iframe['data-src']
-        if "voe.sx" in data_src:
-            voe_links.append(data_src)
-            return voe_links
+        return [iframe_tag['src']]
+    return None
 
 
 def get_title(soup):
+    episodes = {}
     og_title = soup.find('meta', property='og:title')
-    return og_title['content']
+    name = og_title['content'] + " -"
+    try:
+        episode_options = soup.select('.pmovie__series-select select')[0].find_all('option')
+    except IndexError:
+        for iframe in soup.find_all('iframe', attrs={'data-src': True}):
+            data_src = iframe['data-src']
+            if "voe.sx" in data_src:
+                episodes[og_title['content']] = data_src
+                return episodes
+        episodes[og_title['content']] = ""
+        return episodes
+
+
+    for option in episode_options:
+        ep_id = option['value']
+        ep_name = f"{name} {option.text.strip()}"
+
+        link_select = soup.find('select', {'id': ep_id})
+        if link_select:
+            link_option = link_select.find('option')
+            if link_option and link_option.get('value'):
+                episodes[ep_name] = link_option['value']
+
+    return episodes
+
 
 def clear() -> None:
     if platform.system() == "Windows":
--- megakino/src/menu.py
+++ megakino/src/menu.py
@@ -1,5 +1,5 @@
 import npyscreen
-from .common import get_html_from_search, clear, get_megakino_episodes, get_title, get_voe_episodes
+from .common import get_html_from_search, clear, get_megakino_episodes, get_title
 from .download import download
 from .megakino import megakino_get_direct_link
 from .voe import voe_get_direct_link
@@ -9,7 +9,8 @@ from .watch import watch
 
 def main():
     HTML_CONTENT = get_html_from_search()
-    Titles = [get_title(HTML_CONTENT)]
+    episodes = get_title(HTML_CONTENT)
+    titles = list(episodes.keys())
 
     class MegakinoForm(npyscreen.ActionForm):
         def create(self):
@@ -17,7 +18,7 @@ def main():
 
             self.provider = self.add(npyscreen.TitleSelectOne, name="Provider:", max_height=5, values=["Megakino", "VOE"], scroll_exit=True, value=0)
 
-            self.episodes = self.add(npyscreen.TitleMultiSelect, name="Choose Episodes:", values=Titles, scroll_exit=True)
+            self.episodes = self.add(npyscreen.TitleMultiSelect, name="Choose Episodes:", values=titles, scroll_exit=True)
 
         def on_ok(self):
             selected_action = self.action.get_selected_objects()
@@ -25,7 +26,7 @@ def main():
             selected_episodes = self.episodes.get_selected_objects()
 
 
-            titles_link_list = list(selected_episodes)
+            chosen_episodes = list(selected_episodes)
             selected_action = selected_action[0]
             selected_provider = selected_provider[0]
             clear()
@@ -39,18 +40,18 @@ def main():
                         direct_links.append(link)
 
             elif selected_provider == "VOE" or not direct_links:
-                voe_list = get_voe_episodes(HTML_CONTENT)
-                if voe_list:
-                    for episode in voe_list:
+                urls = [episodes[name] for name in chosen_episodes]
+                if urls:
+                    for episode in urls:
                         link = voe_get_direct_link(episode)
                         direct_links.append(link)
-
+            print(direct_links)
             if selected_action == "Watch":
-                watch(direct_links, titles_link_list)
+                watch(direct_links, chosen_episodes)
             elif selected_action == "Download":
-                download(direct_links, titles_link_list)
+                download(direct_links, chosen_episodes)
             elif selected_action == "Syncplay":
-                syncplay(direct_links, titles_link_list)
+                syncplay(direct_links, chosen_episodes)
 
             self.parentApp.switchForm(None)
 
